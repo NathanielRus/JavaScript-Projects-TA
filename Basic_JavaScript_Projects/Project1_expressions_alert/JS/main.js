b2ac4cd9ad4daaa31a2435052879b311aea0c6bb
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2021-11-30 16:52:54 -0700

    Basic JavaScript

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var Family="The Russell\'s", Dad="Brain", Mom="Alice", Son="Nathan";
+var Family=Family.fontcolor("yellow")
+var Dad=Dad.fontcolor("blue"); //dad will be blue
+var Mom=Mom.fontcolor("red"); //mom will be red
+var Son=Son.fontcolor("green");
+document.write(Family);
+document.write(3*3);
