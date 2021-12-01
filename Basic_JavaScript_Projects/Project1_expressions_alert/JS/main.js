def26f16bb64bba0d3c4053b9b7f82eec6ba77ba
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2021-12-01 12:59:28 -0700

    Fixed code

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,11 +1,15 @@
 var Family="The Russell\'s", Dad="Brain", Mom="Alice", Son="Nathan";
-var Family=Family.fontcolor("yellow")
+var Family=Family.fontcolor("yellow");
 var Dad=Dad.fontcolor("blue"); //dad will be blue
 var Mom=Mom.fontcolor("red"); //mom will be red
 var Son=Son.fontcolor("green");
 document.write(Family);
 document.write(3*3);
 window.alert("Hello there!");
-var Past="The Family\'s past is long"
-+" But not too long";
-document.write(Past);
+var Past="The Family\'s past is long";
+var Future=" But not too long";
+document.write(Past+Future);
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  }
