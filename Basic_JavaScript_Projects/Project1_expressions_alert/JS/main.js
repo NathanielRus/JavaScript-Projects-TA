23e78ab78152e9967053c5fc9d01d97e7fe99c5d
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2021-12-01 11:57:03 -0700

    Upload Project

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -5,3 +5,7 @@ var Mom=Mom.fontcolor("red"); //mom will be red
 var Son=Son.fontcolor("green");
 document.write(Family);
 document.write(3*3);
+window.alert("Hello there!");
+var Past="The Family\'s past is long"
++" But not too long";
+document.write(Past);
