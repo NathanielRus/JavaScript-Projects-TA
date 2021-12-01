d5060d292af5727fc4c9aaf34db8d5ac61798ec6
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2021-12-01 13:09:12 -0700

    Second Project

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,9 +1,9 @@
-function displayDate() {
+function displayDate() { //first function
     document.getElementById("date").innerHTML = Date();
   }
 
-  function myFunction() {
+  function myFunction() { //second function
       var sentence ="I am learning";
-      sentence +=" a lot from this book!";
+      sentence +=" a lot from this book!"; //adds to the sentence var
       document.getElementById("Concatenate").innerHTML=sentence;
   }
